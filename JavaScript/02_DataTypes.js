158d985df24da89039cc15354006dc5c2f3a9323
Akshay Mehta <nkanal38@gmail.com>
2025-10-10 11:46:10 +0530

    types of datatype

---
+++ JavaScript/02_DataTypes.js
@@ -0,0 +1,19 @@
+let name = "akshay"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (it's an object)
+// undefined => not defined any value
+// symbol => unique
+
+// object
+
+console.log(typeof name); //it tells the type of data use
+
+console.log(typeof null); //undefined
+console.log(typeof undefined); // object
+
