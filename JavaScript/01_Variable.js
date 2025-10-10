a2b65c59bc64fbd9df49d9c37996fc1243521b5b
Akshay Mehta <nkanal38@gmail.com>
2025-10-10 11:24:34 +0530

    add the js variable file

---
+++ JavaScript/01_Variable.js
@@ -0,0 +1,21 @@
+const accountId = 1342
+let accountEmail = "nkanal38@gmail.com"
+var accountPassword = "12345"
+accountCity = "New York"
+let accountState;
+
+//accountId = 2 //Not allowed
+
+// console.log(accountId); // one way to print 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+/*
+try not to use var
+because of issue in block scope and functional scope
+*/
+
+/*
+in js we can declare variable without any datatype
+*/
